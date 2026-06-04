d4611676c93106427c08ae1667f101ef30915f42
Ahamed Nismi <mnanismi2000@gmail.com>
2026-06-04 16:58:08 +0530

    smoker percentage between gender

--- SQL/01_analysis/patient_analysis.sql
+++ SQL/01_analysis/patient_analysis.sql
@@ -19,3 +19,23 @@
     FROM framingham_cleaned 
     GROUP BY gender
 
+
+  -- Among individuals with a 10-year CHD risk, smoking behavior shows clear variation by gender.
+  -- Males exhibit a higher smoking prevalence and greater average cigarette consumption compared to females, 
+  -- while females demonstrate a higher proportion of non-smokers within the high-risk group.
+  --   This suggests smoking is a strong contributing risk factor for CHD, although it is not the only determinant, 
+  --   as other lifestyle and biological factors may also influence risk
+  
+  SELECT 
+      gender,
+      COUNT(*) AS total_people,
+      ROUND(SUM(CASE WHEN is_smoker='Yes' THEN 1 ELSE 0 END)  * 100.0 / COUNT(*),2) AS smokers,
+      ROUND(SUM(CASE WHEN is_smoker='No' THEN 1 ELSE 0 END)  * 100.0 / COUNT(*) ,2)AS non_smokers,
+      ROUND(AVG(cigarettes_per_day)::numeric,2) AS avg_cigarettes_per_day
+  FROM framingham_cleaned
+  WHERE chd_risk_10yr = 'Yes'
+  GROUP BY gender;
+
+
+     SELECT * FROM  framingham_cleaned
+
